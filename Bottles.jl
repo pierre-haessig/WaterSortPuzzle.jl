676fea8617744cfc5a47210e42715b7573166df4
Pierre Haessig <pierre.haessig@crans.org>
2023-12-02 16:34:00 +0100

    first version of children enumeration which looks correct

--- Bottles.jl
+++ Bottles.jl
@@ -24,8 +24,20 @@ end
 """is Bottle a full?"""
 isfull(a::Bottle) = a[1] != NC
 
+"count number of empty slots on top of Bottle a"
+function count_empty(a::Bottle)
+    # index of first non empty slot:
+    itop = findfirst(ai -> ai != NC, a)
+    if itop === nothing
+        return length(a)
+    else
+        return itop-1
+    end
+end
+
 """color at the top of the bottle, excluding empty slots. returns NC if empty"""
 function top_color(a::Bottle)
+    # index of first non empty slot:
     itop = findfirst(ai -> ai != NC, a)
     if itop === nothing
         return  NC
@@ -34,18 +46,28 @@ function top_color(a::Bottle)
     end
 end
 
-"number of empty slots on top of Bottle a"
-function n_empty(a::Bottle)
-    c = 0
-    for ai in a
-        if ai == NC
-            c += 1
+"""color number of slots of the top color. returns bottle length if empty"""
+function count_top_color(a::Bottle)
+    # index of first non empty slot:
+    itop = findfirst(ai -> ai != NC, a)
+    if itop === nothing
+        return length(a)
+    else
+        # top color
+        c = a[itop]
+        i_second = findfirst(ai -> ai != c, a[itop:end])
+        if i_second === nothing
+            return length(a) - (itop-1)
         else
-            break
+            return i_second-1
         end
     end
-    return c
 end
+
+# tests
+@assert count_top_color(Bottle([NC,1,1,2])) == 2
+@assert count_top_color(Bottle([NC,1,1,1])) == 3
+@assert count_top_color(Bottle([NC,NC,NC])) == 3
 
 color_format(i) = i!=NC ? string(i; base=16) : "_"#␣∅
 
@@ -112,34 +134,46 @@ e=Bottle([1,3,2])
 println(a)
 # Canonical order: from smallest to greatest, using lexicographic order
 println("Bottles:", [a,b,c,d,e])
-println("Bottles:", sort([a,b,c,d,e]), "sorted")
+println("Bottles:", sort([a,b,c,d,e]), " after sort")
 
 """list of children nodes of Bottles `v`"""
 function children(v::Bottles)
     c = Vector{Bottles}()
-    for i in eachindex(v), j in eachindex(v)
-        if i==j
+    for i_source in eachindex(v), i_dest in eachindex(v)
+        if i_source==i_dest
             continue
         end
-        source = v[i]
-        dest = v[j]
-        top_source = top_color(source)
-        top_dest = top_color(dest)
+        source = v[i_source]
+        dest = v[i_dest]
+        c_source = top_color(source)
+        c_dest = top_color(dest)
 
-        if top_dest != NC && top_source != top_dest
+        if c_source == NC
             continue
         end
 
-        n_empty_dest = n_empty(dest)
+        if c_dest != NC && c_source != c_dest
+            continue
+        end
+
+        n_empty_dest = count_empty(dest)
         if n_empty_dest == 0
             continue
         end
 
+        n_c_source = count_top_color(source)
+        # number of slots to transfer
+        n_transfer = min(n_empty_dest, n_c_source)
+
         #Create new Vector of Bottles with transfered colors
         child = [copy(a) for a in v]
-        for k in 1:n_empty_dest # false: min in available color in source
-            child[i][k] = NC # false: add offest of n_empty_source
-            child[j][k] = top_source
+
+        # index of first non empty slot:
+        itop_source = findfirst(ai -> ai != NC, source)
+
+        for k in 1:n_transfer # false: min in available color in source
+            child[i_source][itop_source-1+k] = NC
+            child[i_dest][(n_empty_dest-n_transfer)+k] = c_source
         end
         push!(c,child)
     end
